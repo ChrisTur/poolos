83e5739386a7c234cbe0f834405a971bc67de7d0
Christopher Turner <christopher.l.turner@aa.com>
2026-06-17 13:18:23 -0500

    updated invoice / app to include payment options

---
+++ prisma/migrations/20260617181230_add_payment_handles/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE "Company" ADD COLUMN     "cashAppHandle" TEXT,
+ADD COLUMN     "paypalHandle" TEXT,
+ADD COLUMN     "venmoHandle" TEXT,
+ADD COLUMN     "zelleEmail" TEXT,
+ADD COLUMN     "zellePhone" TEXT;
